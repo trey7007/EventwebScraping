95847c9bada8761fc728c1faa71d81554903c4bf
Trey Vincent <treyvincent@hotmail.com>
2024-04-20 14:34:25 -0500

    Including email functionality

---
+++ SendingEmail/send.py
@@ -0,0 +1,27 @@
+
+from dotenv import load_dotenv
+import os
+import smtplib
+from email.message import EmailMessage
+
+load_dotenv()
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+
+contacts = ['treyvincent@hotmail.com']
+
+msg = EmailMessage()
+msg['Subject'] = 'Upcoming events, Madison!'
+msg['From'] = EMAIL_ADDRESS
+
+msgtxt = open('emailcontents.txt','r').read()
+
+msg.set_content(msgtxt, subtype='html')
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    for c in contacts:
+        msg['To'] = c
+        smtp.send_message(msg)
